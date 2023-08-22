50ec22430a8cfe5bcb6cb051fcf7ba1fcef1d07e
Wayne keya <keyawayne1@gmail.com>
2023-08-22 05:39:00 +0300

    assinment done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
-def return_evens(num_list):
-    pass
+def return_evens(sequence):
+    return [num for num in sequence if num % 2 == 0]
 
-def make_exclamation(sentence_list):
-    pass
+# Test the function
+result = return_evens([0, 1, 3, 5, 7, 8, 9])
+print(result)  # Output: [0, 8]
+
+
+def make_exclamation(sentences):
+    return [sentence + '!' for sentence in sentences]
+
